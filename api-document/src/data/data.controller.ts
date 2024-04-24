0acbe7be88c8d890995403269fe48d8a27b123cc
Folk_Jrk <jarmekorntec@gmail.com>
2024-04-25 00:04:31 +0700

    All completed

--- api-document/src/data/data.controller.ts
+++ api-document/src/data/data.controller.ts
@@ -2,7 +2,6 @@ import { Controller, Post, Body, Req } from '@nestjs/common';
 import { DataService } from './data.service';
 import { EncryptDataDto } from './dto/encrypt-data.dto';
 import { DecryptDataDto } from './dto/decrypt-data.dto';
-import { Request } from 'express';
 
 @Controller()
 export class DataController {
@@ -10,11 +9,10 @@ export class DataController {
 
   @Post('get-encrypt-data')
   async encryptData(@Body() encryptDataDto: EncryptDataDto, @Req() req: Request): Promise<any> {
-    const privateKey = process.env.PRIVATE_KEY;
+    const privateKeyRSA = process.env.PRIVATE_KEY;
     const aesKey = this.dataService.generateRandomAESKey();
-    this.dataService.storeAESKeyInSession(req, aesKey); // Store AES key in session
     const encryptedPayload = this.dataService.encryptWithAES(encryptDataDto.payload, aesKey);
-    const encryptedAESKey = this.dataService.encryptWithPrivateKey(aesKey, privateKey);
+    const encryptedAESKey = this.dataService.encryptWithPrivateKey(aesKey, privateKeyRSA);
     return {
       successful: true,
       data: { data1: encryptedAESKey, data2: encryptedPayload }
@@ -23,15 +21,15 @@ export class DataController {
 
   @Post('get-decrypt-data')
   async decryptData(@Body() decryptDataDto: DecryptDataDto, @Req() req: Request): Promise<any> {
-    const publicKey = process.env.PUBLIC_KEY;
-    const aesKeyFromSession = this.dataService.getAESKeyFromSession(req); // Retrieve AES key from session
-    const decryptedAESKey = this.dataService.decryptWithPrivateKey(decryptDataDto.data1, aesKeyFromSession, publicKey);
-    const decryptedPayload = this.dataService.decryptWithAES(decryptDataDto.data2, aesKeyFromSession || decryptedAESKey);
+    const publicKeyRSA = process.env.PUBLIC_KEY;
+    const decryptedAESKey = this.dataService.decryptWithPublicKey(decryptDataDto.data1, publicKeyRSA);
+    const decryptedPayload = this.dataService.decryptWithAES(decryptDataDto.data2, decryptedAESKey);
+    
     return {
       successful: true,
       error_code: null,
       data: {
-        payload: decryptedAESKey,
+        payload: decryptedPayload,
       }
     };
   }
